408a2400ad5b047f1a4d5313e283db3c203ce4fb
Rommel Rico <me@rommelrico.com>
2023-05-24 21:03:13 -0700

    :sparkles: s4: Adding a route using a Link component

--- coffee-app/src/app/coffee-store/[slug]/page.tsx
+++ coffee-app/src/app/coffee-store/[slug]/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 interface Props { params: {slug: string} }
 
 const CoffeeStore = (props: Props) => {
   const {slug} = props.params;
   return (
-    <div>Coffee Store Page {slug}</div>
+    <div>
+      <h1>Coffee Store Page {slug}</h1>
+      <Link href="/">Back to Home</Link>
+    </div>
   )
 };
 
