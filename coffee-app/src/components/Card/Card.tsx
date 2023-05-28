44fb2a543b789bd5d758f3a5eca682c1d4868a0d
Rommel Rico <me@rommelrico.com>
2023-05-28 16:06:24 -0700

    :sparkles: s7: Card component styling

--- coffee-app/src/components/Card/Card.tsx
+++ coffee-app/src/components/Card/Card.tsx
@@ -1,3 +1,6 @@
+import styles from "./Card.module.css";
+
+import cls from "classnames";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,9 +14,13 @@ const Card = (props: Props) => {
   const { name, imageUrl, href } = props;
 
   return (
-    <Link href={href}>
-      <h2>{name}</h2>
-      <Image src={imageUrl} alt="Image" width={260} height={160} />
+    <Link href={href} className={styles.cardLink}>
+      <div className={cls("glass", styles.container)}>
+        <div className={styles.cardHeaderWrapper}><h2 className={styles.cardHeader}>{name}</h2></div>
+        <div className={styles.cardImageWrapper}>
+          <Image className={styles.cardImage} src={imageUrl} alt="Image" width={260} height={160} />
+        </div>
+      </div>
     </Link>
   );
 };
