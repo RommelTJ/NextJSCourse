5b90ba94a0af272247ee958f039412a29db22b98
Rommel Rico <me@rommelrico.com>
2023-06-01 07:27:02 -0700

    :sparkles: s7: Add coffee stores on the page

--- coffee-app/src/app/page.tsx
+++ coffee-app/src/app/page.tsx
@@ -6,6 +6,8 @@ import styles from "./styles.module.css";
 import Banner from "@/components/Banner";
 import Card from "@/components/Card/Card";
 
+import coffeeStores from "../data/coffee-stores.json";
+
 export default function Home() {
 
   const handleOnBannerBtnClick = () => {
@@ -19,10 +21,17 @@ export default function Home() {
         <Image src="/static/hero-image.png" alt="hero image" width={700} height={400} />
       </div>
       <div className={styles.cardLayout}>
-        <Card name="DarkHorse Coffee 1" href="/coffee-store/darkhorse-coffee-1" imageUrl="/static/hero-image.png" className={styles.card} />
-        <Card name="DarkHorse Coffee 2" href="/coffee-store/darkhorse-coffee-2" imageUrl="/static/hero-image.png" className={styles.card} />
-        <Card name="DarkHorse Coffee 3" href="/coffee-store/darkhorse-coffee-3" imageUrl="/static/hero-image.png" className={styles.card} />
-        <Card name="DarkHorse Coffee 4" href="/coffee-store/darkhorse-coffee-4" imageUrl="/static/hero-image.png" className={styles.card} />
+        { coffeeStores.map(s => {
+          return (
+            <Card
+              key={s.id}
+              name={s.name}
+              href={`/coffee-store/${s.id}`}
+              imageUrl={s.imgUrl}
+              className={styles.card}
+            />
+          )
+        }) }
       </div>
     </main>
   )
