2291fd7ad404e0c11288347f3cb64186dcec8ed3
Rommel Rico <me@rommelrico.com>
2023-06-04 22:36:43 -0700

    :sparkles: s7: Removes dummy dynamic link

--- coffee-app/src/app/coffee-store/[slug]/page.tsx
+++ coffee-app/src/app/coffee-store/[slug]/page.tsx
@@ -15,13 +15,12 @@ export async function generateStaticParams() {
 
 const CoffeeStore = ({ params }: Props) => {
   const { slug } = params;
-  const store: CoffeeStore = coffeeStoreData.find(s => s.id == Number(slug)) || {id: -1, name: "unknown", address: "unknown"};
+  const store: CoffeeStore = coffeeStoreData.find(s => s.id == Number(slug))!;
   const { name, address } = store;
   return (
     <div>
       <h1>Coffee Store Page {slug}</h1>
       <div><Link href="/">Back to Home</Link></div>
-      <div><Link href={`/coffee-store/2324tsg`}>Go to dynamic</Link></div>
       <div>
         <p>Name: {name}</p>
         <p>Address: {address}</p>
