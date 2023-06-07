73044a00f3670c6ed5bfa35370c29ae57e776dc8
Rommel Rico <me@rommelrico.com>
2023-06-06 22:16:26 -0700

    :sparkles: s7: Adds fetchPlacePhotos

--- coffee-app/src/lib/coffee-stores.ts
+++ coffee-app/src/lib/coffee-stores.ts
@@ -1,19 +1,20 @@
 import {FoursquareLocation} from "@/models/FoursquareLocation";
 import {CoffeeStore} from "@/models/CoffeeStore";
+import {FoursquarePhoto} from "@/models/FoursquarePhoto";
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `${process.env.FOURSQUARE_API_KEY}`
+  }
+};
 
 const getUrlForCoffeeStores = (query: string, latLong: string, fields: string, limit: number) => {
   return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${latLong}&fields=${fields}&limit=${limit}`
 }
 
 export const fetchFoursquareCoffeeStores = async () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `${process.env.FOURSQUARE_API_KEY}`
-    }
-  };
-
   const query = "coffee%20shop";
   const latLong = "32.7067015%2C-117.13199";
   const fields = "fsq_id%2Cname%2Clocation";
@@ -26,6 +27,17 @@ export const fetchFoursquareCoffeeStores = async () => {
   const jsonData = await res.json();
   return jsonData.results as FoursquareLocation[];
 };
+
+export const fetchPlacePhotos = async (id: string) => {
+  const res = await fetch(`https://api.foursquare.com/v3/places/${id}/photos?limit=1`, options);
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+  const jsonData = await res.json();
+  const photos = jsonData as FoursquarePhoto[];
+  return photos[0];
+}
 
 export const foursquareToCoffeeStore = (loc: FoursquareLocation): CoffeeStore => {
   return {
