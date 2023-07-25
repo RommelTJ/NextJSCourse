2ed914bd23366a5aba3c6e19d7db1e9a97523deb
Rommel Rico <me@rommelrico.com>
2023-07-24 21:38:24 -0700

    :sparkles: s9: Dog breed exercise

--- coffee-app/src/app/api/route.ts
+++ coffee-app/src/app/api/route.ts
@@ -5,3 +5,11 @@ export async function GET(request: NextRequest) {
   const data = { hello: "world" };
   return NextResponse.json(data);
 }
+
+interface PostParams { breed: string; }
+export async function POST(request: NextRequest) {
+  const body: PostParams = await request.json();
+  const { breed } = body;
+  const data = { message: `I love ${breed}` };
+  return NextResponse.json(data);
+}
