a44456771d4acc679caf55f1ba84f6c43bfa3abd
Rommel Rico <me@rommelrico.com>
2023-09-23 15:58:23 -0700

    :sparkles: s14: Card component image error

--- netflix/src/components/Card/Card.tsx
+++ netflix/src/components/Card/Card.tsx
@@ -6,12 +6,14 @@ import { useState } from "react";
 import Image from "next/image";
 
 interface Props {
-  imgUrl: string;
-  size: "small"|"medium"|"large";
+  imgUrl?: string;
+  size?: "small"|"medium"|"large";
 }
 
+const IMAGE_DEFAULT = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80";
+
 const Card = (props: Props) => {
-  const { imgUrl = "/static/clifford.webp", size = "medium" } = props;
+  const { imgUrl = IMAGE_DEFAULT, size = "medium" } = props;
   const [imgSrc, setImgSrc] = useState(imgUrl);
 
   const classMap = {
@@ -20,7 +22,7 @@ const Card = (props: Props) => {
     small: styles.smItem,
   };
 
-  const handleOnError = () => setImgSrc("/static/clifford.webp");
+  const handleOnError = () => setImgSrc(IMAGE_DEFAULT);
 
   return (
     <div className={styles.container}>
