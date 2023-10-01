768dc7f485dad15fb59ebec3c5963a9ad942adf8
Rommel Rico <me@rommelrico.com>
2023-09-30 21:10:50 -0700

    :sparkles: s14: Invoke Youtube API

--- netflix/src/lib/videos.ts
+++ netflix/src/lib/videos.ts
@@ -1,17 +1,21 @@
-import videoData from "../data/videos.json";
 import { Video } from "@/models/Video";
 
-// process.env.YOUTUBE_API_KEY in .env.local
+interface YoutubeVideo {
+  snippet: {title: string, thumbnails: {high: {url: string}}};
+  id: {videoId: string}
+}
 
-export const getVideos = (): Promise<Video[]> => {
-  return new Promise<Video[]>((resolve, reject) => {
-    const videos = videoData.items.map((item) => {
-      return {
-        title: item.snippet.title,
-        imgUrl: item.snippet.thumbnails.high.url,
-        id: item?.id?.videoId,
-      };
-    });
-    resolve(videos);
+export const getVideos = async (): Promise<Video[]> => {
+  const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+  const response = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=disney%20trailer&key=${YOUTUBE_API_KEY}`
+  );
+  const data = await response.json();
+  return data.items.map((item: YoutubeVideo) => {
+    return {
+      title: item.snippet.title,
+      imgUrl: item.snippet.thumbnails.high.url,
+      id: item?.id?.videoId,
+    };
   });
 };
