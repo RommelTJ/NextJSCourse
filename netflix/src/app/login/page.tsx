ea74edaa6714060bca5653bd6189e4942d0249e6
Rommel Rico <me@rommelrico.com>
2023-10-04 21:57:43 -0700

    :sparkles: s15: Sign In Header component

--- netflix/src/app/login/page.tsx
+++ netflix/src/app/login/page.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./login.module.css";
+
 const Login = () => {
   return (
     <div>
-      Netflix SignIn
+      <header>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/static/netflix.svg"
+                alt="Netflix logo"
+                width={128}
+                height={34}
+              />
+            </div>
+          </Link>
+        </div>
+      </header>
     </div>
   );
 };
