99f6996fbd42e77344259d735422fd89d7b32052
Rommel Rico <me@rommelrico.com>
2024-02-06 08:07:51 -0800

    :sparkles: s16: ISR implementation (sort of)

--- netflix/src/app/video/[videoId]/page.tsx
+++ netflix/src/app/video/[videoId]/page.tsx
@@ -5,17 +5,44 @@ import Modal from "react-modal";
 import {useRouter} from "next/navigation";
 import clsx from "classnames";
 
-const Video = ({ params }: { params: { videoId: string } }) => {
-  const router = useRouter();
+export type YoutubeVideo = {
+  title: string;
+  publishTime: string;
+  description: string;
+  channelTitle: string;
+  viewCount: number;
+}
 
-  const video = {
+// i.e. getStaticProps
+async function getVideo(): Promise<YoutubeVideo> {
+  // TODO: Fetch this from the YouTube API
+  const video: YoutubeVideo = {
     title: "Hi cute dog",
     publishTime: "1990-01-01",
     description:
-      "A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?",
+      "A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?",
     channelTitle: "Paramount Pictures",
     viewCount: 10000,
   };
+
+  // revalidate does Incremental Static Regeneration (ISR)
+  // const res = await fetch(`https://.../posts`, { next: { revalidate: 60 } })
+  // const data = await res.json()
+  // return data.posts
+
+  return Promise.resolve(video)
+}
+
+// true: (default) Dynamic segments not included in generateStaticParams are generated on demand.
+// false: Dynamic segments not included in generateStaticParams will return a 404.
+export const dynamicParams = true;
+export async function generateStaticParams() {
+  return [{ videoId: 'mYfJxlgR2jw' }, { videoId: '4zH5iYM4wJo' }, { videoId: "KCPEHsAViiQ" }]
+}
+
+const Video = async ({ params }: { params: { videoId: string } }) => {
+  const router = useRouter();
+  const video = await getVideo();
   const { title, publishTime, description, channelTitle, viewCount } = video;
 
   return (
