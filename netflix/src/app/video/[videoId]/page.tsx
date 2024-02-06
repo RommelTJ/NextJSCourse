ebbfc3379bdf1387909bd1c6aa58fec0616e3173
Rommel Rico <me@rommelrico.com>
2024-02-05 20:09:02 -0800

    :sparkles: s16: Modal component structure

--- netflix/src/app/video/[videoId]/page.tsx
+++ netflix/src/app/video/[videoId]/page.tsx
@@ -3,9 +3,20 @@
 import styles from "./Video.module.css";
 import Modal from "react-modal";
 import {useRouter} from "next/navigation";
+import clsx from "classnames";
 
 const Video = ({ params }: { params: { videoId: string } }) => {
   const router = useRouter();
+
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    description: "A big red dog that is super cute, can he get any bigger?",
+    channelTitle: "Paramount Pictures",
+    viewCount: 10000,
+  };
+  const { title, publishTime, description, channelTitle, viewCount } = video;
+
   return (
     <div>
       <Modal
@@ -24,6 +35,26 @@ const Video = ({ params }: { params: { videoId: string } }) => {
           src={`https://www.youtube.com/embed/${params.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder="0"
         ></iframe>
+
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
