71f6fc35a535f7ae6541b5a49a4e6560ffac7df0
Rommel Rico <me@rommelrico.com>
2024-02-12 07:53:15 -0800

    :sparkles: s17: User Roles with JWT

--- netflix/src/lib/db/hasura.ts
+++ netflix/src/lib/db/hasura.ts
@@ -7,7 +7,10 @@ async function fetchGraphQL(operationsDoc: string, operationName: string, variab
   const result = await fetch(`${process.env.NEXT_PUBLIC_HASURA_ADMIN_URL}`, {
     method: "POST",
     headers: {
-      "x-hasura-admin-secret": `${process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET}`,
+      "X-Hasura-Role": "user",
+      "X-Hasura-User-Id": "notrommel",
+      // Authorization: "Bearer <token>",
+      "X-Hasura-Admin-Secret": `${process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET}`,
     },
     body: JSON.stringify({
       query: operationsDoc,
