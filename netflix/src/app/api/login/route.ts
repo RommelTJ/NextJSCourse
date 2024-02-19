8f25808a589695fffd9202df1fe2929414cb39b2
Rommel Rico <me@rommelrico.com>
2024-02-19 09:58:59 -0800

    :sparkles: s18: Login API Create JWT

--- netflix/src/app/api/login/route.ts
+++ netflix/src/app/api/login/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import {magicAdmin} from "@/lib/magic";
+import jwt from "jsonwebtoken";
+
 
 export async function POST(request: NextRequest) {
   const auth = request.headers.get('Authorization');
@@ -7,10 +9,21 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ done: false });
   }
   const didToken = auth.substring(7); // Bearer token
-  console.log("didToken", didToken);
-
   const metadata = await magicAdmin.users.getMetadataByToken(didToken);
-  console.log({ metadata });
+  const jwtToken = jwt.sign(
+    {
+      ...metadata,
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+      "https://hasura.io/jwt/claims": {
+        "x-hasura-allowed-roles": ["user", "admin"],
+        "x-hasura-default-role": "user",
+        "x-hasura-user-id": `${metadata.issuer}`,
+      },
+    },
+    process.env.HASURA_JWT_SECRET_KEY || "",
+  );
+  console.log({ jwtToken });
 
   return NextResponse.json({ done: true });
 }
