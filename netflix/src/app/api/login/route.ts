b3de1f76ed9ef939bce2370055beccdb4cb1218b
Rommel Rico <me@rommelrico.com>
2024-02-19 09:34:22 -0800

    :sparkles: s18: Login API Create Auth Header

--- netflix/src/app/api/login/route.ts
+++ netflix/src/app/api/login/route.ts
@@ -1,5 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
+  const auth = request.headers.get('Authorization');
+  if (!auth) {
+    return NextResponse.json({ done: false });
+  }
+  const didToken = auth.substring(7);
+  console.log("didToken", didToken);
   return NextResponse.json({ done: true });
 }
