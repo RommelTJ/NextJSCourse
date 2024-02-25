0d4a1c04699369dd9b7572e1fc0fafb5a5955db8
Rommel Rico <me@rommelrico.com>
2024-02-25 13:33:50 -0800

    :sparkles: s19: Build Stats with Request Cookies

--- netflix/src/app/api/stats/route.ts
+++ netflix/src/app/api/stats/route.ts
@@ -2,5 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 
 
 export async function POST(request: NextRequest) {
+  const tokenCookie = request.cookies.get("token");
+  if (!tokenCookie) {
+    return NextResponse.json({}, { status: 403 });
+  }
   return NextResponse.json({ msg: "it works" });
 }
