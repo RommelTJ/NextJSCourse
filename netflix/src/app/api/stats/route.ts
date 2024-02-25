66252cea314392e575448a1034f04b3185a4722c
Rommel Rico <me@rommelrico.com>
2024-02-25 14:00:27 -0800

    :sparkles: s19: Use query string for video ID

--- netflix/src/app/api/stats/route.ts
+++ netflix/src/app/api/stats/route.ts
@@ -1,15 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 import { findVideoIdByUser } from "@/lib/db/hasura";
 
 
+interface PostParams { videoId: string; }
+
 export async function POST(request: NextRequest) {
   const tokenCookie = request.cookies.get("token");
   if (!tokenCookie) return NextResponse.json({}, { status: 403 });
-  const decodedToken = jwt.verify(tokenCookie.value, process.env.HASURA_JWT_SECRET_KEY || "");
-  const userId = "didToken";
-  const videoId = "4zH5iYM4wJo";
+  const decodedToken: JwtPayload | string = jwt.verify(tokenCookie.value, process.env.HASURA_JWT_SECRET_KEY || "");
+
+  if (typeof decodedToken === "string") return NextResponse.json({}, { status: 400 });
+  const tokenPayload = decodedToken as JwtPayload;
+  const body: PostParams = await request.json();
+  const userId = tokenPayload.issuer || "";
+  const videoId = body.videoId;
   const findVideoId = await findVideoIdByUser(tokenCookie.value, userId, videoId);
   return NextResponse.json({ msg: "it works", decodedToken, findVideoId });
 }
