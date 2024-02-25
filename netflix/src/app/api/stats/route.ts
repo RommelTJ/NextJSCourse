cec2c7633035c85eafe094274bb8fe9511c193b8
Rommel Rico <me@rommelrico.com>
2024-02-25 13:36:55 -0800

    :sparkles: s19: Stats API JWT verification

--- netflix/src/app/api/stats/route.ts
+++ netflix/src/app/api/stats/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
+import jwt from "jsonwebtoken";
 
 
 export async function POST(request: NextRequest) {
   const tokenCookie = request.cookies.get("token");
-  if (!tokenCookie) {
-    return NextResponse.json({}, { status: 403 });
-  }
-  return NextResponse.json({ msg: "it works" });
+  if (!tokenCookie) return NextResponse.json({}, { status: 403 });
+  const decodedToken = jwt.verify(tokenCookie.value, process.env.HASURA_JWT_SECRET_KEY || "");
+  return NextResponse.json({ msg: "it works", decodedToken });
 }
