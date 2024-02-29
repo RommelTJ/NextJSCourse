8ab1fa80453709f901207f00990c0fb795a43179
Rommel Rico <me@rommelrico.com>
2024-02-28 21:01:20 -0800

    :sparkles: s19: Tweak stats API GET request

--- netflix/src/app/api/stats/route.ts
+++ netflix/src/app/api/stats/route.ts
@@ -9,18 +9,36 @@ export async function POST(request: NextRequest) {
   const tokenCookie = request.cookies.get("token");
   if (!tokenCookie) return NextResponse.json({}, { status: 403 });
   const decodedToken: JwtPayload | string = jwt.verify(tokenCookie.value, process.env.HASURA_JWT_SECRET_KEY || "");
-
   if (typeof decodedToken === "string") return NextResponse.json({}, { status: 400 });
   const tokenPayload = decodedToken as JwtPayload;
   const body: PostParams = await request.json();
   const userId = tokenPayload.issuer || "";
   const { videoId, favorited, watched = true} = body;
   const token = tokenCookie.value;
-  const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
-
-  const response = doesStatsExist
+  const hasVideoStats = await findVideoIdByUser(token, userId, videoId);
+  const response = hasVideoStats
     ? await updateStats(token, { favorited, userId, watched, videoId })
     : await insertStats(token, { favorited, userId, watched, videoId });
 
-  return NextResponse.json({ msg: "it works", response });
+  return NextResponse.json({ data: response });
+}
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const videoId = searchParams.get("videoId");
+  if (!videoId) return NextResponse.json({}, { status: 400 });
+
+  const tokenCookie = request.cookies.get("token");
+  if (!tokenCookie) return NextResponse.json({}, { status: 403 });
+  const decodedToken: JwtPayload | string = jwt.verify(tokenCookie.value, process.env.HASURA_JWT_SECRET_KEY || "");
+  if (typeof decodedToken === "string") return NextResponse.json({}, { status: 400 });
+  const tokenPayload = decodedToken as JwtPayload;
+  const token = tokenCookie.value;
+  const userId = tokenPayload.issuer || "";
+  const hasVideoStats = await findVideoIdByUser(token, userId, videoId);
+  if (hasVideoStats) {
+    return NextResponse.json({ data: hasVideoStats });
+  } else {
+    return NextResponse.json({ data: hasVideoStats }, { status: 404 });
+  }
 }
