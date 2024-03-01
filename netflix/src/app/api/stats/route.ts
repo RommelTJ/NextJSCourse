2a52c5ed44bc375f16916140e2e25f5204b84148
Rommel Rico <me@rommelrico.com>
2024-02-29 20:20:49 -0800

    :sparkles: s19: More stats api tweaks

--- netflix/src/app/api/stats/route.ts
+++ netflix/src/app/api/stats/route.ts
@@ -3,18 +3,28 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { findVideoIdByUser, updateStats, insertStats } from "@/lib/db/hasura";
 
 
+const getTokenFromCookie = (request: NextRequest): { token: string; userId: string; } | undefined => {
+  const tokenCookie = request.cookies.get("token");
+  if (tokenCookie) {
+    const decodedToken: JwtPayload | string = jwt.verify(tokenCookie.value, process.env.HASURA_JWT_SECRET_KEY || "");
+    if (typeof decodedToken === "string") return undefined;
+    const tokenPayload = decodedToken as JwtPayload;
+    const token = tokenCookie.value;
+    const userId = tokenPayload.issuer || "";
+    return { token, userId };
+  } else {
+    return undefined;
+  }
+}
+
 interface PostParams { videoId: string; favorited: number; watched: boolean; }
 
 export async function POST(request: NextRequest) {
-  const tokenCookie = request.cookies.get("token");
-  if (!tokenCookie) return NextResponse.json({}, { status: 403 });
-  const decodedToken: JwtPayload | string = jwt.verify(tokenCookie.value, process.env.HASURA_JWT_SECRET_KEY || "");
-  if (typeof decodedToken === "string") return NextResponse.json({}, { status: 400 });
-  const tokenPayload = decodedToken as JwtPayload;
   const body: PostParams = await request.json();
-  const userId = tokenPayload.issuer || "";
   const { videoId, favorited, watched = true} = body;
-  const token = tokenCookie.value;
+  const cookieData = getTokenFromCookie(request);
+  if (!cookieData) return NextResponse.json({}, { status: 403 });
+  const { token, userId } = cookieData;
   const hasVideoStats = await findVideoIdByUser(token, userId, videoId);
   const response = hasVideoStats
     ? await updateStats(token, { favorited, userId, watched, videoId })
@@ -27,14 +37,9 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const videoId = searchParams.get("videoId");
   if (!videoId) return NextResponse.json({}, { status: 400 });
-
-  const tokenCookie = request.cookies.get("token");
-  if (!tokenCookie) return NextResponse.json({}, { status: 403 });
-  const decodedToken: JwtPayload | string = jwt.verify(tokenCookie.value, process.env.HASURA_JWT_SECRET_KEY || "");
-  if (typeof decodedToken === "string") return NextResponse.json({}, { status: 400 });
-  const tokenPayload = decodedToken as JwtPayload;
-  const token = tokenCookie.value;
-  const userId = tokenPayload.issuer || "";
+  const cookieData = getTokenFromCookie(request);
+  if (!cookieData) return NextResponse.json({}, { status: 403 });
+  const { token, userId } = cookieData;
   const hasVideoStats = await findVideoIdByUser(token, userId, videoId);
   if (hasVideoStats) {
     return NextResponse.json({ data: hasVideoStats });
